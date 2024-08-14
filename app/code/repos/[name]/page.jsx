743bc036ea07220857f5795cc915f68d8004d284
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-14 18:21:58 +0200

    Implement repo and repo dirs components

---
+++ app/code/repos/[name]/page.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import Repo from "@/app/components/Repo";
+import RepoDirs from "@/app/components/RepoDirs";
+
+const RepoPage = ({ params: { name } }) => {
+  return (
+    <div className="card">
+      <Link href="/code/repos" className="btn btn-back">
+        Back to repositories
+      </Link>
+      <Repo name={name} />
+      <RepoDirs name={name} />
+    </div>
+  );
+};
+
+export default RepoPage;
