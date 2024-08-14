834d040830f3db98718e24c917af2f0bdc36b067
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-14 12:05:03 +0200

    Add some routing

---
+++ app/about/page.jsx
@@ -0,0 +1,17 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Traversy Media</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid libero
+        aliquam laboriosam fugiat, ipsum, blanditiis minima accusamus eos neque
+        esse officiis placeat unde itaque soluta tenetur! Velit fugiat earum
+        porro perferendis optio repellendus aliquam reprehenderit labore fuga
+        mollitia. In repellendus, eligendi dolores iusto ut necessitatibus
+        laborum quia non possimus asperiores?
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,15 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Out Team</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+        velit quo quibusdam, quasi, sed quidem, expedita est provident obcaecati
+        adipisci atque! Beatae fugit non quasi pariatur dignissimos accusantium
+        hic expedita.
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
